bf28e651cd2287b7935790e5c2c0be173e612b67
blueh0rse <110935049+blueh0rse@users.noreply.github.com>
2023-12-02 23:30:21 +0100

    add echo messages

--- generator.sh
+++ generator.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+echo ""
+echo "---------------"
+echo "---GENERATOR---"
+echo "---------------"
+echo ""
+
 ALICE="alice"
-BOB="bob"
 PUBLIC="public"
 
+echo "[0] Starting environment preparation..."
 # check if alice/ exist
 if [ ! -d "$ALICE" ]; then
     if ! mkdir "$ALICE"; then
@@ -13,18 +19,6 @@ else
     # clean
     rm "./$ALICE"/*
 fi
-
-# check if bob/ exist
-if [ ! -d "$BOB" ]; then
-    if ! mkdir "$BOB"; then
-        echo "Error while creating $BOB"
-        exit 1
-    fi
-else
-    # clean
-    rm "./$BOB"/*
-fi
-
 # check if public/ exist
 if [ ! -d "$PUBLIC" ]; then
     if ! mkdir "$PUBLIC"; then
@@ -35,16 +29,25 @@ else
     # clean
     rm "./$PUBLIC"/*
 fi
+echo "[0] Done!"
 
 # 1. Generate the DH group
+echo "[1] Generating DH group in $ALICE/dhparams.pem..."
 openssl genpkey -genparam -algorithm dhx -pkeyopt dh_rfc5114:3 -out "$ALICE/dhparams.pem"
+echo "[1] Done!"
 
 # 2. Generate long-term keypair
+echo "[2] Generating long-term keypair in $ALICE/privA.pem..."
 openssl genpkey -paramfile "$ALICE/dhparams.pem" -out "$ALICE/privA.pem"
+echo "[2] Done!"
 
 # 3. Extract long-term public key
+echo "[3] Extracting long-term public key in $ALICE/pubA.pem..."
 openssl pkey -in "$ALICE/privA.pem" -pubout -out "$ALICE/pubA.pem"
+echo "[3] Done!"
 
 # 4. Publish the dhparams.pem file and pubA.pem
+echo "[4] Publishing dhparams.pem & pubA.pem in $PUBLIC..."
 cp "$ALICE/dhparams.pem" "$PUBLIC"
 cp "$ALICE/pubA.pem" "$PUBLIC"
+echo "[4] Done!"
